1a9d813a78f2e3a050fd8314db70749d04977c74
qbkira <qbuouo@gmail.com>
2023-06-25 21:18:30 +0800

    update plugin meta

--- nonebot_plugin_helltide/__init__.py
+++ nonebot_plugin_helltide/__init__.py
@@ -12,13 +12,13 @@ from nonebot.adapters.onebot.v11 import (
 import datetime
 
 __plugin_meta__ = PluginMetadata(
-    name="helltide",
-    description="一个Diablo4的helltide和世界boss的提醒小助手",
+    name="Diablo4地狱狂潮boss提醒小助手",
+    description="支持订阅，在D4继续上班",
     # BEGIN: 7d7f3c7b5d4a
     usage="这是一个diablo4插件，可以订阅游戏中的事件，如boss刷新、地狱潮汐等，\n当事件即将发生时会自动提醒订阅用户。使用方法：\n1. 订阅事件：d4订阅 boss/helltide\n2. 取消订阅事件：d4取消订阅 boss/helltide\n3. 查询订阅列表：d4查询订阅",
     # END: 7d7f3c7b5d4a
     homepage="https://github.com/QBkira/nonebot-plugin-helltide",
-    type="library",
+    type="application",
     config=Config,
     supported_adapters={"~onebot.v11"},
     extra={"author": "qbkira"},
